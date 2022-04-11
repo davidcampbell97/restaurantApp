42b6f531cf07de9aa8d7040ffd0cff0168b1077d
davidcampbell97 <davidcampbell97@hotmail.com>
2022-04-11 12:55:39 +0100

    initial router functionality

--- Controllers/restaurantRouter.js
+++ Controllers/restaurantRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', function(req, res){
 
-res.send('Hello and welcome to the restaurant application');
+res.send('Hello test and welcome to the restaurant application');
 
 })
 
@@ -13,7 +13,14 @@ router.use(function(req, res){
 
     res.status(404);
     res.type('text/plain');
-    res.send('404 not found');
+  res.send('404 not found');
+})
+
+
+router.use(function(err, req, res, next) 
+{ res.status(500); 
+    res.type('text/plain'); 
+    res.send('Internal Server Error.');
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const nedb = require('nedb');
 const db = new nedb({filename: 'restaurant.db', autoload: true});
-const router = ('./Controllers/restaurantRouter.js');
+const router = require('./Controllers/restaurantRouter');
+
+
+
 app.use('/', router);
 
 
 
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000. Ctrl^c to quit.'); })
 
 console.log('db created');
